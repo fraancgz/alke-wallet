cb26c9c836dcbc4e90d2a4899fe55855c8bae677
fraan_cgz <franecg1989@gmail.com>
2026-01-26 18:25:56 -0300

    :hammer: Agregado el atributo saldo al array de usuariosRegistrados. Se modificaron las variables a inglés y se realizaron mejoras visuales en Home y Login

--- js/home.js
+++ js/home.js
@@ -1,29 +1,33 @@
-// Al cargar el Home...
-// Estas variables son "globales" para el archivo que se cargue
-const usuariosRegistrados = JSON.parse(localStorage.getItem('usuarios_db')) || [];
-const mailActivo = localStorage.getItem('usuarioActivo');
-// console.log(usuariosRegistrados)
-// console.log(mailActivo)
+// Declaro las variables fuera (vacías) para que sean globales
+let registeredUsers
+let currentUser
+let userBalance = 0
+let userIndex = -1
+let textDeposit
+let actualBalanceUser
 
-
+// Cargo la vista deposito para que quede por defecto y asigno respectivos valores a variables globales
 document.addEventListener("DOMContentLoaded", () => {
-    // Cargo la vista deposito para que quede por defecto
-    cargarVista('deposit.html');
+
+    registeredUsers = JSON.parse(localStorage.getItem('dbUsers')) || []
+    currentUser = localStorage.getItem('currentUser')
+    userIndex = registeredUsers.findIndex(user => user.mail === currentUser)
+
+    // Cargo la vista por defecto, el salgo se cargara en el fetch.
+    renderView('deposit.html');
+
 });
 
-if (mailActivo) {
-    // Busco los datos del usuario en el array principal
-    const datosUsuario = usuariosRegistrados.find(user => user.mail === mailActivo);
-    const usernameHome = document.querySelector("#usernameHome")
-
-    if (datosUsuario) {
-        usernameHome.textContent = datosUsuario.username
-    } else
-        console.log("no")
+// Funcion mostrar el saldo solo si currentUser y actualBalanceUser estan cargados previamente
+function showBalance() {
+    // console.log(actualBalanceUser + " " +currentUser)
+    if (currentUser && actualBalanceUser)
+        formatBalance(actualBalanceUser, registeredUsers[userIndex].balance)
+    else
+        console.log("Usuario o balance no han sido establecidos aún")
 }
 
 const depositForm = document.querySelector("#depositForm")
-let userBalance = 0
 
 // Esucho todos los eventos de tipo submit y filtro segun el id de formulario para saber cual es al que se hizo click
 document.addEventListener("submit", (e) => {
@@ -34,73 +38,83 @@ document.addEventListener("submit", (e) => {
 
     if (id === "depositForm") {
         // console.log("Lógica de depósito");
-        logicaFormularioDeposito()
+        loginFormDeposit()
     } else if (id === "sendForm") {
         console.log("Lógica de envío");
     }
 })
 
-function logicaFormularioDeposito() {
+function loginFormDeposit() {
     const depositForm = document.querySelector("#depositForm")
-    const saldoActualDeposito = document.querySelector("#saldoActualDeposito")
-    const textDeposit = document.querySelector("#textDeposit")
+    actualBalanceUser = document.querySelector("#actualBalanceUser")
+    textDeposit = document.querySelector("#textDeposit")
     const alertDeposit = document.querySelector("#alertDeposit")
-
-    formatearSaldo(saldoActualDeposito, userBalance)
 
     const dataForm = new FormData(depositForm)
     const objectJson = Object.fromEntries(dataForm.entries())
 
-    userBalance += parseInt(objectJson.montoDeposito)
+    userBalance += parseInt(objectJson.depositAmount)
+    registeredUsers[userIndex].balance += userBalance
 
-    mostrarMensajeDeAlerta(alertDeposit, textDeposit, "Depósito realizado con éxito")
+    localStorage.setItem('dbUsers', JSON.stringify(registeredUsers))
 
-    restablecerFormularioDeposito()
-    formatearSaldo(saldoActualDeposito, userBalance);
+    showAlertMessage(alertDeposit, textDeposit, "Depósito realizado con éxito")
+    restoreDepositForm()
+    formatBalance(actualBalanceUser, registeredUsers[userIndex].balance);
 }
 
 // Funcion que muestra un mensaje de alerta y lo restablece luego de 5 segundos
-function mostrarMensajeDeAlerta(alertElement, textElement, message) {
+function showAlertMessage(alertElement, textElement, message) {
     alertElement.classList.remove('d-none')
     alertElement.classList.add('d-inline')
-    alertElement.classList.add("d-flex", "align-items-center" ,"justify-content-center")
+    alertElement.classList.add("d-flex", "align-items-center", "justify-content-center")
     textElement.textContent = message
 
     setTimeout(() => {
-            restablecerAlert(alertElement)
+        restoreAlert(alertElement)
     }, 3000);
 }
 
 // Funcion que agrega la clase display none al alert ingresado como atributo
-function restablecerAlert(alertElement) {
+function restoreAlert(alertElement) {
     alertElement.classList.add('d-none')
     alertElement.classList.remove('d-inline')
 }
 
-function formatearSaldo(elemento, valorFinal) {
-
-    elemento.textContent = new Intl.NumberFormat('es-CL', {
+function formatBalance(element, newBalance) {
+    element.textContent = new Intl.NumberFormat('es-CL', {
         style: 'currency',
         currency: 'CLP',
         minimumFractionDigits: 0
-    }).format(valorFinal);
+    }).format(newBalance);
 }
 
-function restablecerFormularioDeposito() {
-    document.querySelector("#montoDeposito").value = ""
+function restoreDepositForm() {
+    document.querySelector("#depositAmount").value = ""
     document.activeElement.blur()
 }
 
-function cargarVista(vista) {
+function renderView(view) {
     // Mediante la funcion fetch se inicia el proceso de comunicación de red para obtener la pagina solicitada
     // En resumen, busca el archivo HTML
-    fetch(vista)
+    fetch(view)
         .then(response => {
             if (!response.ok) throw new Error("Error al cargar la vista")
             return response.text() // Convierto la respuesta a texto/html  
         })
         .then(html => {
             contenedorDinamico.innerHTML = html // Injecto el Html en el contenedor
+           
+            const usernameHome = document.querySelector("#usernameHome")
+
+            if (userIndex != -1) {
+                // Establesco el nombre de usuario
+                usernameHome.textContent = registeredUsers[userIndex].username
+                
+                // Ejecuto la funcion para mostrar el saldo al momento de cargar el HTML
+                actualBalanceUser = document.querySelector("#actualBalanceUser")
+                showBalance();
+            }
         })
         .catch(error => {
             console.log(error)
